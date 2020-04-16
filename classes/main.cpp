d0e70d6c62c57bf47122e4e748ea55b457bf8c05
Raisa Primerova <48605821+RayRedGoose@users.noreply.github.com>
2020-04-15 21:28:39 -0600

    Update main.cpp

--- classes/main.cpp
+++ classes/main.cpp
@@ -1,8 +1,37 @@
 #include <iostream>
 #include <string>
+#include "users.hpp"
 
 using namespace std;
 
 int main() {
+    Users user;
     
+    string username, password, first_name, last_name;
+    string full_name, acc_info;
+    
+    cout << "Username: ";
+    getline(cin, username);
+    
+    cout << "Password: ";
+    getline(cin, password);
+    
+    user.set_username(username);
+    user.set_password(password);
+    user.set_admin(username);
+    
+    cout << "First Name: ";
+    getline(cin, first_name);
+    
+    cout << "Last Name: ";
+    getline(cin, last_name);
+    
+    user.set_first_name(first_name);
+    user.set_last_name(last_name);
+    
+    full_name = user.get_full_name();
+    acc_info = user.get_account_info();
+    
+    cout << "Hi, " + full_name + ". ";
+    cout << "Your account is " + acc_info + ". ";
 };
