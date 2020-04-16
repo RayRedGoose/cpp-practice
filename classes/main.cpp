f1e03cb8a87dd3275f65fd271a6f4d867ecd60f5
Raisa Primerova <48605821+RayRedGoose@users.noreply.github.com>
2020-04-15 21:48:25 -0600

    Update class initialization

--- classes/main.cpp
+++ classes/main.cpp
@@ -5,8 +5,6 @@
 using namespace std;
 
 int main() {
-    Users user;
-    
     string username, password, first_name, last_name;
     string full_name, acc_info;
     
@@ -16,9 +14,8 @@ int main() {
     cout << "Password: ";
     getline(cin, password);
     
-    user.set_username(username);
-    user.set_password(password);
-    user.set_admin(username);
+    Users *user = new Users(username, password);
+    user->set_admin(username);
     
     cout << "First Name: ";
     getline(cin, first_name);
@@ -26,12 +23,15 @@ int main() {
     cout << "Last Name: ";
     getline(cin, last_name);
     
-    user.set_first_name(first_name);
-    user.set_last_name(last_name);
+    user->set_first_name(first_name);
+    user->set_last_name(last_name);
     
-    full_name = user.get_full_name();
-    acc_info = user.get_account_info();
+    full_name = user->get_full_name();
+    acc_info = user->get_account_info();
     
     cout << "Hi, " + full_name + ". ";
     cout << "Your account is " + acc_info + ". ";
+    
+    delete user;
+    return 0;
 };
